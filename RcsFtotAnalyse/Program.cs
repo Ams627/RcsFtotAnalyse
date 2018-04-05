1ab254a1d6be1c592f78212050348aac45188f29
Adrian Sims <adriansims62781@gmail.com>
2018-04-06 00:42:42 +0100

    Add project files.

---
+++ RcsFtotAnalyse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RcsFtotAnalyse
+{
+    using System.Xml.Linq;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var filename = "RCS_R_T_180115_03106.xml";
+                var doc = XDocument.Load(filename);
+                var r1 = doc.Descendants("FTOT").Select(x => x.Descendants("LicenseeType")
+                    .Where(licenseeType => licenseeType.Attribute("lt")?.Value == "00001" &&
+                           licenseeType.Descendants("Licensee")
+                                .Select(licensee=>licensee.Descendants("Channel").Select(channelNode=>channelNode.Attribute("ch")?.Value).Contains("00004")
+                           ))).
+
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add RcsFtotAnalyse/Program.cs
+git add RcsFtotAnalyse/App.config
+git add RcsFtotAnalyse/Properties/AssemblyInfo.cs
+git add RcsFtotAnalyse/RcsFtotAnalyse.csproj
+git add RcsFtotAnalyse.sln
+git add .gitignore
